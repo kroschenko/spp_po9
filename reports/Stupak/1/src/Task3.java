51f661c4a2cb0ab23acb22c337a61bfb2af1b553
SkillIssue <99391484+Papirosa225@users.noreply.github.com>
2024-02-28 16:10:07 +0300

    Update Task3.java

--- reports/Stupak/1/src/Task3.java
+++ reports/Stupak/1/src/Task3.java
@@ -1,17 +1,18 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
         System.out.println("Введите символ и кол-во повторений: ");
-
-        System.out.println(repeat(scanner.next().charAt(0), scanner.nextInt()));
+        System.out.println(repeat(scanner.next().charAt(0), scanner.nextDouble()));
     }
 
-    static String repeat(char ch, int repeat) {
+    static String repeat(char ch, double repeat) {
         String str = "";
 
-        for (int i = 0; i < repeat; i++) {
+        for (int i = 0; i < (int)repeat; i++) {
             str += ch;
         }
 
